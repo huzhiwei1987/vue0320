a1a4bee360cc7fba1b5297306bbab7ef0ffb8f1d
huzhiwei <329697878@qq.com>
2018-03-20 16:57:32 +0800

    feat: 项目第一天，app组件tabbar 和 header,home页面完成轮播图和六宫格图标

---
+++ src/components/tabbar/ShopcarContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div><h2>ShopcarContainer</h2></div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
 // 入口文件
 import Vue from 'vue'
 
-import {Header} from 'mint-ui'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+import {Header, Swipe, SwipeItem} from 'mint-ui'
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
 import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
+
+
+import router from './router.js'
 
 import app from './App.vue'
 
 var vm = new Vue({
   el: '#app',
-  render: c => c(app)
+  render: c => c(app),
+  router
 })
